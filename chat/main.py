c186f0cbf45d29a1e55906968e6ca6e88b57c09e
piyushmishraDS <40821421+piyushmishraDS@users.noreply.github.com>
2023-03-21 16:36:41 +0530

    Update main.py

--- chat/main.py
+++ chat/main.py
@@ -43,7 +43,7 @@ st.header("GPTflix is like chatGPT for movie reviews!🍿\n")
 st.header("Thanks for visiting GPTflix! It's been a fun experiment, with over 4000 unique users over four weeks and an average of 10 questions per user while the site was online! Perhaps we will be back some time...🍿\n")
 
 # Define the name of the index and the dimensionality of the embeddings
-index_name = "400kmovies"
+index_name = "1kmovies"
 dimension = 1536
 
 pineconeindex = pinecone.Index(index_name)
