70dd651290ec2a66920d480c7cdcd69d92b551f9
piyushmishraDS <40821421+piyushmishraDS@users.noreply.github.com>
2023-03-21 16:59:27 +0530

    Update main.py

--- chat/main.py
+++ chat/main.py
@@ -43,7 +43,7 @@ st.set_page_config(page_title="GPTflix", page_icon="🍿", layout="wide")
 st.header("Thanks for visiting GPTflix! It's been a fun experiment, with over 2500 unique users over two weeks and an average of 10 questions per user while the site was online! Perhaps we will be back some time...🍿\n")
 
 # Define the name of the index and the dimensionality of the embeddings
-index_name = "400kmovies"
+index_name = "1kmovies"
 dimension = 1536
 
 pineconeindex = pinecone.Index(index_name)
